d4176f381fd3ff008c665456fabc56808c60f1b4
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-10-07 20:56:40 -0300

    feat: explicacao sobre trim()

---
+++ exercicios/resolucao/respostas-exercicios-introdutorios.js
@@ -0,0 +1,16 @@
+// 1. Se ontem foi quarta-feira, amanhã será...
+// let array = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado']
+// console.log(array[0]) //domingo
+// console.log(array[1]) //segunda-feira
+// console.log('O número de itens do meu array é :' + array.length)
+// // o tamanho do meu array - 1, sempre será o índice máximo do meu array
+// console.log('Índice máximo do array ' + Number(array.length - 1))
+let email = '       daniel@gmail.com '
+
+if (email.trim() == 'daniel@gmail.com') {
+    console.log('Email com trim ' + '"' + email.trim() + '"')
+    console.log('E-mail válido!')
+} else {
+    console.log('E-mail inválido!')
+}
+
